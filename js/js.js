e00e8801d8c8c5d7841b0d804d35fe52496fc3da
“CD-HA” <CD-HA>
2019-11-05 15:33:18 -0500

    “ultima.actualizacion”

--- js/js.js
+++ js/js.js
@@ -4,3 +4,20 @@ var nav  = document.getElementById ("nav")
 btnmenu.addEventListener("click", function(){
     nav.classList.toggle("mostrar")
 })
+
+import { Component } from '@angular/core';
+import { Options } from 'ng5-slider';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent  {
+  value: number = 10;
+  highValue: number = 1000;
+  options: Options = {
+    floor: 0,
+    ceil: 100
+  };
+}
